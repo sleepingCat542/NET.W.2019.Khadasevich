2761f866f7c53c000fa84ef40ff14aa5a00b1b9a
Yuliana <yuki.sai@mail.ru>
2019-09-03 15:14:13 +0300

    2 task

---
+++ NET.W.2019.Khadasevich.04/Task2/NET.W.2019.Khadasevich.04.02/NET.W.2019.Khadasevich.04.002/Ieee754.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NET.W._2019.Khadasevich._04._002
+{
+    public static class Ieee754
+    {
+        /// <summary>
+        /// Returns the IEEE 754 representation of double number.
+        /// </summary>
+        /// <param name="number">Number to convert.</param>
+        /// <returns>String that contains the IEEE 754 representation of double number.</returns>
+        public static string GetIeee754Representation(this double number)
+        {
+            return ConvertLongToBinaryString(new Union(number).longNumber);
+        }
+
+        /// <summary>
+        /// Converts long number to binary.
+        /// </summary>
+        /// <param name="value">Number to convert.</param>
+        /// <returns>String that contains binary representation of the number.</returns>
+        private static string ConvertLongToBinaryString(long value)
+        {
+            const int doubleSize = 64;
+            var result = new char[doubleSize];
+
+            for (int i = 0; i < doubleSize; i++)
+            {
+                long mask = 1L << i;
+                result[doubleSize - i - 1] = (value & mask) == 0 ? '0' : '1';
+            }
+
+            return new string(result);
+        }
+
+        /// <summary>
+        /// Structure all fields of which start at the same location in memory.
+        /// So longNumber field will contain doubleNumber IEEE 754 representation.
+        /// </summary>
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Union
+        {
+            [FieldOffset(0)]
+            private readonly double doubleNumber;
+
+            [FieldOffset(0)]
+            public readonly long longNumber;
+
+            public Union(double number)
+            {
+                longNumber = 0;
+                doubleNumber = number;
+            }
+        }
+
+    }
+}
---
+++ NET.W.2019.Khadasevich.04/Task2/NET.W.2019.Khadasevich.04.02/NET.W.2019.Khadasevich.04.02.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NET.W.2019.Khadasevich.04.02")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NET.W.2019.Khadasevich.04.02")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b7c7390-89eb-435e-91d0-ee081f7ad09c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
