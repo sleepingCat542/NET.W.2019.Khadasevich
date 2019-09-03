2b9a558e4258a6c28c46b1a0dddc6963b29f23a6
Yuliana <yuki.sai@mail.ru>
2019-09-04 00:39:56 +0300

    add 4 task

---
+++ NET.W.2019.Khadasevich.11/Task4/NET.W.2019.Khadasevich.11.Task4/NET.W.2019.Khadasevich.11.Task4/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace NET.W._2019.Khadasevich._11.Task4
+{
+    class FibonachiClass
+    {
+        /// <summary>
+		/// Recursive method for fibonacci numbers
+		/// </summary>
+		/// <param name="n">count of fibonacci numbers</param>
+        static int FibonachiRecursion(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return FibonachiRecursion(n - 1) + FibonachiRecursion(n - 2);
+            }
+        }
+
+        /// <summary>
+		/// Method searches and prints fibonacci numbers
+		/// </summary>
+        static void Fibonachi()
+        {
+            Console.WriteLine("Enter count of numbers:");
+            int count = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+                Console.WriteLine($"{FibonachiRecursion(i)}");
+        }
+    }
+}
