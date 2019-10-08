cd471d75ff7f7ae6254b830b9cc420daab68194c
Yuliana <yuki.sai@mail.ru>
2019-10-08 21:27:04 +0300

    delete

--- NET.W.2019.Khadasevich.24/NET.W.2019.Khadasevich.24.01/NET.W.2019.Khadasevich.24.01/Modules/MyModule.cs
+++ NET.W.2019.Khadasevich.24/NET.W.2019.Khadasevich.24.01/NET.W.2019.Khadasevich.24.01/Modules/MyModule.cs
@@ -8,24 +8,24 @@ namespace NET.W._2019.Khadasevich._24._01.Modules
 {
     //public class MyModule : IHttpModule
     //{
-    //    //public void Init(HttpApplication app)
-    //    //{
-    //    //    app.BeginRequest += HandleBeginRequest;
-    //    //    app.EndRequest += HandleEndRequest;
-    //    //}
-    //    //private void HandleBeginRequest(object src, EventArgs args)
-    //    //{
-    //    //    HttpContext context = HttpContext.Current;
-    //    //    context.Response.Write(
-    //    //        "<div style='color:red;'>BeginRequest</div>");
-    //    //}
-    //    //private void HandleEndRequest(object src, EventArgs args)
-    //    //{
-    //    //    HttpContext context = HttpContext.Current;
-    //    //    context.Response.Write(
-    //    //        "<div style='color:red;'>EndRequest</div>");
-    //    //}
-    //    //public void Dispose()
-    //    //{ }
+    //    public void Init(HttpApplication app)
+    //    {
+    //        app.BeginRequest += HandleBeginRequest;
+    //        app.EndRequest += HandleEndRequest;
+    //    }
+    //    private void HandleBeginRequest(object src, EventArgs args)
+    //    {
+    //        HttpContext context = HttpContext.Current;
+    //        context.Response.Write(
+    //            "<div style='color:red;'>BeginRequest</div>");
+    //    }
+    //    private void HandleEndRequest(object src, EventArgs args)
+    //    {
+    //        HttpContext context = HttpContext.Current;
+    //        context.Response.Write(
+    //            "<div style='color:red;'>EndRequest</div>");
+    //    }
+    //    public void Dispose()
+    //    { }
     //}
 }
